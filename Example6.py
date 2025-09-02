98054ab2e1902c5ec0f4a123cfc812fdfd34b613
Aboud Barsekh Onji <aboudonji@gmail.com>
2025-09-02 14:52:21 -0600

    add example6

---
+++ Example6.py
@@ -0,0 +1,20 @@
+# Ejemplo de los diferentes tipos de datos en Python
+# Ejemplo6.py
+quantity = int(input())
+price = float(input())
+total = quantity * price
+print(total)
+# Si se ingresa 3 y 19.99, la salida será 59.97
+# la diferencia entre int y float es que int es un numero entero y float es un numero decimal
+# =======================================================================
+# Otro ejemplo: 
+# la multiplicacion y la potencia
+m=input("ingresa un numero: ")
+m=int(m)
+f=input("ingresa un numero: ")
+f=float(f)
+X=f*m
+print(X)
+Y=f**m
+print(Y)
+# =======================================================================
