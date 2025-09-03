2a16dc124ac2e061a7aaf82d9f5e459a26a4b571
Aboud Barsekh Onji <aboudonji@gmail.com>
2025-09-03 13:38:43 -0600

    add Example7.py

--- Example6.py
+++ Example6.py
@@ -7,7 +7,7 @@ print(total)
 # Si se ingresa 3 y 19.99, la salida será 59.97
 # la diferencia entre int y float es que int es un numero entero y float es un numero decimal
 # =======================================================================
-# Otro ejemplo: 
+# Otro ejemplo:
 # la multiplicacion y la potencia
 m=input("ingresa un numero: ")
 m=int(m)
