44bb99d577102b30845af5d880d9b9aff0dbb8b6
Aboud Barsekh Onji <aboudonji@gmail.com>
2025-10-29 16:28:35 -0600

    Example 30 added

---
+++ Example30.py
@@ -0,0 +1,20 @@
+# Este código ordena una lista de listas usando el algoritmo de burbuja.
+calificaciones = [
+    [8, 5, 10, 7],
+    [9, 9, 6, 8],
+    [10, 7, 7, 9]
+]
+print("Calificaciones antes de ordenar:")
+for fila in calificaciones:
+    print(fila)
+    
+for fila in calificaciones:
+    n = len(fila)
+    for i in range(n):
+        for j in range(0, n-i-1): 
+            if fila[j] > fila[j+1]:
+                fila[j], fila[j+1] = fila[j+1], fila[j]
+
+print("Calificaciones después de ordenar:")
+for fila in calificaciones:
+    print(fila)
